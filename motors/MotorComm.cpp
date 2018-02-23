4adc91ac8c5f25be52ff5bcb13a9969a1381df54
Jsteen18 <jes130330@utdallas.edu>
2018-02-23 10:05:26 -0600

    stuff

---
+++ motors/MotorComm.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jes130330 on 2/16/2018.
+//
+
+#include "MotorComm.h"
---
+++ motors/MotorComm.h
@@ -0,0 +1,14 @@
+//
+// Created by jes130330 on 2/16/2018.
+//
+
+#ifndef TEAM555_MOTORCOMM_H
+#define TEAM555_MOTORCOMM_H
+
+
+class MotorComm {
+
+};
+
+
+#endif //TEAM555_MOTORCOMM_H
